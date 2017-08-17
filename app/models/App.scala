731f4086b1829abe67b1094f9747017e854b07db
joymufeng <joymufeng@gmail.com>
2017-08-17 13:29:27 +0800

    添加首页标题设置

--- app/models/App.scala
+++ app/models/App.scala
@@ -6,7 +6,10 @@ package models
 object App {
   val version = "1.0.0"
   var siteSetting = SiteSetting(
-    "PlayScala社区", "http://www.playscala.cn", "/assets/images/logo.png", List.empty[Link], "/assets/favicon.ico",
+    "PlayScala社区",
+    "http://www.playscala.cn",
+    "PlayScala社区 - Play Framework & Scala & Akka 技术交流平台",
+    "/assets/images/logo.png", List.empty[Link], "/assets/favicon.ico",
     "Play社区,Play框架社区,Play开发社区,Play Framework社区,Play Framework教程,Play Framework入门,Play Framework指南,Scala社区,Scala入门,Scala指南,Scala教程,Scala开发",
     "PlayScala社区是一个以Scala和Play Framework为中心的开发社区，为Play Framework开发者提供一个学习和交流平台。通过社区的力量，不断地沉淀、整理Play Framework相关知识，让初学者更快地融入Play Framework大家庭。"
   )
--- app/models/Setting.scala
+++ app/models/Setting.scala
@@ -1,4 +1,4 @@
 package models
 
 case class Link(title: String, url: String, target: String)
-case class SiteSetting(name: String, url: String, logo: String, links: List[Link], favicon: String, seoKeyword: String, seoDescription: String)
+case class SiteSetting(name: String, url: String, indexTitle: String, logo: String, links: List[Link], favicon: String, seoKeyword: String, seoDescription: String)
