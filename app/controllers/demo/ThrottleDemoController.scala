466969355acd500f719a9c0542f21593e686bb07
joymufeng <joymufeng@gmail.com>
2018-06-10 16:41:50 +0800

    调整代码样式

--- app/controllers/demo/ThrottleDemoController.scala
+++ app/controllers/demo/ThrottleDemoController.scala
@@ -17,13 +17,13 @@ class ThrottleDemoController @Inject()(cc: ControllerComponents, mongo: Mongo)(i
     Source
       .queue[ThrottledRequest](100, OverflowStrategy.backpressure).throttle(1, 1.second, 1, ThrottleMode.shaping)
       .toMat(Sink.foreach{ r =>
-    // 处理未超时请求
-    if (System.currentTimeMillis() - r.time <= 15000) {
-      r.promise.success(false)
-    } else {
-      r.promise.success(true)
-    }
-  })(Keep.left).run()
+        // 处理未超时请求
+        if (System.currentTimeMillis() - r.time <= 15000) {
+          r.promise.success(false)
+        } else {
+          r.promise.success(true)
+        }
+      })(Keep.left).run()
 
   // 只有通过限速器(sourceQueue)的请求才会被执行
   def throttle[A](action: Action[A]) = Action.async(action.parser) { request =>
