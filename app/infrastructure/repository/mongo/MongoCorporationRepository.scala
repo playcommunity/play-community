15865f44fd34839c8d0d2d4e55900d4b182dac9c
uuzimu <zimudata@aliyun.com>
2019-11-07 23:59:01 +0800

    重构CorporationController类

---
+++ app/infrastructure/repository/mongo/MongoCorporationRepository.scala
@@ -0,0 +1,47 @@
+package infrastructure.repository.mongo
+
+import cn.playscala.mongo.Mongo
+import infrastructure.repository.CorporationRepository
+import javax.inject.{Inject, Singleton}
+import models.{Corporation, Resource}
+import play.api.libs.json.Json.obj
+import play.api.libs.json.{JsObject, Json}
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+
+@Singleton
+class MongoCorporationRepository @Inject()(mongo: Mongo) extends CorporationRepository {
+
+  /**
+    * 根据Id构建领域实体。
+    * @param id 实体标识。
+    * @return 领域实体。
+    */
+  def findById(id: String): Future[Option[Corporation]] = {
+    mongo.findById[Corporation](id)
+  }
+
+  /**
+    * 更新资源
+    */
+  def update(id: String, update: JsObject): Future[Boolean] = {
+    mongo.updateOne[Corporation](Json.obj("_id" -> id), update).map{ ur =>
+      if(ur.getModifiedCount == 1){ true } else { false }
+    }
+  }
+
+  /**
+    * 新增资源
+    */
+  def add(corporation: Corporation): Future[Boolean] = {
+    mongo.insertOne[Corporation](corporation).map{ _ => true}
+  }
+
+  /**
+    * 查询资源列表, 为提高查询效率，忽略资源内容。
+    */
+  def findActiveList(sort: JsObject): Future[List[Corporation]] = {
+    mongo.find[Corporation](obj("active" -> true)).sort(sort).list()
+  }
+}
